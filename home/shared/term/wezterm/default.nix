9c7597ceb5c5db0af0cbd5184957f60a53f898ff
vicrdguez <vicrerdgz@gmail.com>
2024-03-26 17:35:11 +0400

    missing quotes on wezterm themes

--- home/shared/term/wezterm/default.nix
+++ home/shared/term/wezterm/default.nix
@@ -13,46 +13,46 @@
         let colors = config.theme.scheme;
         in
         {
-          text = ''
+          text = /*lua*/''
             return {
                 force_reverse_video_cursor = true,
                 colors = {
-                    foreground = ${colors.fg_bright},
-                    background = ${colors.bg},
+                    foreground = "${colors.fg_bright}",
+                    background = "${colors.bg}",
 
-                    cursor_bg = ${colors.fg},
-                    cursor_fg = ${colors.fg},
-                    cursor_border = ${colors.fg},
+                    cursor_bg = "${colors.fg}",
+                    cursor_fg = "${colors.fg}",
+                    cursor_border = "${colors.fg}",
 
-                    selection_fg = ${colors.fg},
-                    selection_bg = ${colors.selection},
+                    selection_fg = "${colors.fg}",
+                    selection_bg = "${colors.selection}",
 
-                    scrollbar_thumb = ${colors.bg},
-                    split = ${colors.bg},
+                    scrollbar_thumb = "${colors.bg}",
+                    split = "${colors.bg}",
 
                     ansi = {
-                        ${colors.black},
-                        ${colors.red},
-                        ${colors.green},
-                        ${colors.yellow},
-                        ${colors.blue},
-                        ${colors.magenta},
-                        ${colors.cyan},
-                        ${colors.fg}
+                        "${colors.black}",
+                        "${colors.red}",
+                        "${colors.green}",
+                        "${colors.yellow}",
+                        "${colors.blue}",
+                        "${colors.magenta}",
+                        "${colors.cyan}",
+                        "${colors.fg}"
                     },
                     brights = {
-                        ${colors.bg_bright},
-                        ${colors.red_bright},
-                        ${colors.green_bright},
-                        ${colors.yellow_bright},
-                        ${colors.blue_bright},
-                        ${colors.magenta_bright},
-                        ${colors.cyan_bright},
-                        ${colors.fg_bright}
+                        "${colors.bg_bright}",
+                        "${colors.red_bright}",
+                        "${colors.green_bright}",
+                        "${colors.yellow_bright}",
+                        "${colors.blue_bright}",
+                        "${colors.magenta_bright}",
+                        "${colors.cyan_bright}",
+                        "${colors.fg_bright}"
                     },
                     indexed = {
-                        [16] = ${colors.idx_16},
-                        [17] = ${colors.idx_17}
+                        [16] = "${colors.idx_16}",
+                        [17] = "${colors.idx_17}"
                     },
                 }
             }
